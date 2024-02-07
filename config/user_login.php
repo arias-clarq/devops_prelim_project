19bd036139287f7d14722f96222f2143b54d49fa
arias-clarq <clarqarias@gmail.com>
2024-02-08 01:12:36 +0800

    arias-clarq
    -fixes logical error in user_login.php

--- config/user_login.php
+++ config/user_login.php
@@ -8,7 +8,7 @@ if (strpos($username, "@admin") !== false) {
 
     $userSql = "SELECT * FROM `tbl_user` WHERE username = '{$username}' AND password = '{$password}'";
     $result = $conn->query($userSql);
-    
+
     if ($result->num_rows > 0) {
         // activate token
         $_SESSION['token'] = true;
@@ -19,40 +19,46 @@ if (strpos($username, "@admin") !== false) {
         $_SESSION['userID'] = $userID;
 
         header("location: ../admin-files/cms.php");
+    } else {
+        $_SESSION['message'] = 'Incorrect Username or Password';
+        header('location: ../index.php');
     }
 
 } else if (strpos($username, "@student") !== false) {
 
-    $userSql = "SELECT * FROM `tbl_student` WHERE username = '{$username}' AND password = '{$password}' AND statusID = 2";
-    $result = $conn->query($userSql);
+    $checkStatus = "SELECT * FROM `tbl_student` WHERE `username` = '{$username}' AND `password` = '{$password}' AND statusID = 2";
+    $checkStatusResult = $conn->query($checkStatus);
 
-    if ($result->num_rows > 0) {
+    if ($checkStatusResult->num_rows > 0) {
         // activate token
         $_SESSION['token'] = true;
-
+        $userSql = "SELECT * FROM `tbl_student` WHERE username = '{$username}' AND password = '{$password}'";
+        $result = $conn->query($userSql);
         //get userID
         $row = $result->fetch_assoc();
         $studentID = $row["studentID"];
-        
-        $_SESSION['studentID'] = $studentID;
 
-        $studentSql = "SELECT * FROM `tbl_student` INNER JOIN tbl_student_info ON tbl_student.studentID = tbl_student_info.studentID WHERE tbl_student.studentID = {$studentID}";
-        $result2 = $conn->query($studentSql);
-        $row2 = $result2->fetch_assoc();
-        $user = $row2["lName"] . ', ' . $row2["fName"] . ' ' . $row2["mName"];
 
-        if ($result2->num_rows > 0) {
-            $_SESSION['student'] =  $user;
-            header("location: ../student-portal/studentDashboard.php");
+        if ($result ->num_rows > 0) {
+            $_SESSION['studentID'] = $studentID;
+
+            $studentSql = "SELECT * FROM `tbl_student` INNER JOIN tbl_student_info ON tbl_student.studentID = tbl_student_info.studentID WHERE tbl_student.studentID = {$studentID}";
+            $result2 = $conn->query($studentSql);
+            $row2 = $result2->fetch_assoc();
+            $user = $row2["lName"] . ', ' . $row2["fName"] . ' ' . $row2["mName"];
+
+            if ($result2->num_rows > 0) {
+                $_SESSION['student'] = $user;
+                header("location: ../student-portal/studentDashboard.php");
+            }
+        } else {
+            $_SESSION['message'] = 'Incorrect Username or Password';
+            header('location: ../index.php');
         }
-        
-    }else{
+
+    } else {
         $_SESSION['message'] = 'You are not yet enrolled, visit the registar or the accounting for your approval';
         header('location: ../index.php');
     }
 
-    
-} else {
-    $_SESSION['message'] = 'Incorrect Username or Password';
-    header('location: ../index.php');
 }
